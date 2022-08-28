b326202e69cc191555ce97e29dd6b505390aac53
Sergei Rakitin <serjo-66@mail.ru>
2022-08-28 19:02:35 +0400

    Дополнил класс Router, добавил мрашруты

--- application/core/Router.php
+++ application/core/Router.php
@@ -4,6 +4,48 @@ namespace application\core;
 
 class Router
 {
+    protected $routes = [];
+    protected $params = [];
+
+    public function __construct()
+    {
+        $arr = require 'application/config/routes.php';
+        foreach ($arr as $key => $val) {
+            $this->add($key, $val);
+        }
+    }
+
+    public function add($route, $params)
+    {
+        $route = '#^' . $route . '$#';
+        $this->routes[$route] = $params;
+    }
+
+    public function match()
+    {
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                $this->params = $params;
+                return true;
+            }
+        }
+    }
+
+    public function run()
+    {
+        if ($this->match()) {
+            $controller = 'application\controllers\\' . ucfirst($this->params['controller']) . 'Controller.php';
+            if (class_exists($controller)) {
+                echo 'Ok';
+            } else {
+                echo 'Не найден:' . $controller;
+            }
+        } else {
+            echo 'Маршрут не найден';
+        }
+    }
 
 }
+
 
--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
 require 'application/lib/Dev.php';
 
-echo 'Hello';
+use application\core\Router;
+
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class . '.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+$router = new Router();
+
+$router->run();
+
