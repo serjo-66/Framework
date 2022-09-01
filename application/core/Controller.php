722c8ab006475782360bf9400fdf15a99beb5df2
Sergei Rakitin <serjo-66@mail.ru>
2022-09-01 20:42:06 +0400

    Добавил контроль доступа

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -8,10 +8,14 @@ abstract class Controller
 {
     public $route;
     public $view;
+    public $acl;
 
     public function __construct($route)
     {
         $this->route = $route;
+        if (!$this->checkAcl()) {
+            View::errorCode(403);
+        }
         $this->view = new View($route);
         $this->model = $this->loadModel($route['controller']);
     }
@@ -23,4 +27,25 @@ abstract class Controller
             return new $path;
         }
     }
+
+    public function checkAcl()
+    {
+        $this->acl = require 'application/acl/' . $this->route['controller'] . '.php';
+        if ($this->isAcl('all')) {
+            return true;
+        } elseif (isset($_SESSION['authorize']['id']) && $this->isAcl('authorize')) {
+            return true;
+        } elseif (!isset($_SESSION['authorize']['id']) && $this->isAcl('guest')) {
+            return true;
+        } elseif (!isset($_SESSION['admin']) && $this->isAcl('admin')) {
+            return true;
+        }
+        return false;
+    }
+
+    public function isAcl($key)
+    {
+        return in_array($this->route['action'], $this->acl[$key]);
+    }
+
 }
